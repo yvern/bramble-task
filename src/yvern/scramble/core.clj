cb1a172859e7a9c5eab0016d37cdcbd64c82791b
Yuri Vendruscolo <yuri@vw-dilab.com>
2021-07-10 17:10:59 +0100

    ✅ 📝 add docs and further tests for internals

--- src/yvern/scramble/core.clj
+++ src/yvern/scramble/core.clj
@@ -2,23 +2,32 @@
   (:gen-class))
 
 (defn char-left?
+  "given a char histogram (as from frequencies on a string) and a single char,
+   checks if the histogram contains it, and if the count is at least 1."
   [histogram char']
   (and (contains? histogram char')
        (pos? (histogram char'))))
 
 (defn scramble-reducer
+  "given a char histogram (as from frequencies on a string) and a single char,
+   checks if the char is present and its count must be decremented, else ends reduction with false."
   [histogram char']
   (if (char-left? histogram char')
     (update histogram char' dec)
     (reduced false)))
 
 (defn scramble-iter
+  "given two strings, uses the first as scramble letters and the second as target word.
+   iterates over target word, checking if the current char is available from the pool of chars left.
+   by the end returns if the word can be formed from given letters."
   [str1 str2]
   (->> str2 vec
        (reduce scramble-reducer (frequencies str1))
        map?))
 
 (defn scramble?
+  "given two strings, uses the first as scramble letters and the second as target word.
+   first checks for cases where the answer is trivial, then applies scramble if needed."
   [str1 str2]
   (cond
     (= str1 str2) true
--- test/yvern/scramble/core_test.clj
+++ test/yvern/scramble/core_test.clj
@@ -1,16 +1,32 @@
 (ns yvern.scramble.core-test
   (:require [clojure.test :as t]
-            [yvern.scramble.core :refer [scramble? char-left? scramble-reducer scramble-iter]]))
+            [yvern.scramble.core :refer [scramble? char-left? scramble-reducer]]))
 
-(t/deftest inner-calls
+(t/deftest internal-functions
   (t/testing "if there are counts of the given char left"
-    (t/are [histogram char' result] (= result (char-left?  histogram char'))
-      {\a 1, \b 1, \c 1} \b true
-      {\h 1, \e 1, \l 2, \o 1} \l true
-      {\b 1, \i 1, \m 1, \o 1} \m true
-      {\a 1, \b 1, \c 1} \d false
-      {\h 1, \e 1, \l 2, \o 1} \k false
-      {\b 1, \i 1, \m 1, \o 0} \o false)))
+    (t/are [histogram char' result]
+           (= result (char-left?  histogram char'))
+      {\a 1 \b 1 \c 1} \b true
+      {\h 1 \e 1 \l 2 \o 1} \l true
+      {\b 1 \i 1 \m 1 \o 1} \m true
+      {\a 1 \b 1 \c 1} \d false
+      {\h 1 \e 1 \l 2 \o 1} \k false
+      {\b 1 \i 1 \m 1 \o 0} \o false))
+
+  (t/testing "exits iteration early by wrapping in a `reduced`"
+    (t/are [histogram char']
+           ((every-pred reduced? (comp false? deref))
+            (scramble-reducer histogram char'))
+      {\a 1 \b 1 \c 1} \d
+      {\h 1 \e 1 \l 2 \o 1} \k
+      {\b 1 \i 1 \m 1 \o 0} \o))
+
+  (t/testing "does not interfere with other char counts/keys"
+    (t/are [histogram char']
+           (= (dissoc histogram char') (dissoc (scramble-reducer histogram char') char'))
+      {\a 1 \b 1 \c 1} \b
+      {\h 1 \e 1 \l 2 \o 1} \l
+      {\b 1 \i 1 \m 1 \o 1} \m)))
 
 (t/deftest scramble
   (t/testing "equal strings should be the scramble of one another"
@@ -23,9 +39,10 @@
     (t/are [str1 str2] (not (scramble? str1 str2))
       "a" "abc"
       "shorter" "shorters"
-      "qwe" "tyuip")))
+      "qwe" "tyuip"))
 
-
-;; (scramble? “rekqodlw” ”world”) ==> true
-;; (scramble? “cedewaraaossoqqyt” ”codewars”) ==> true
-;; (scramble? “katas”  “steak”) ==> false
+  (t/testing "given specification"
+    (t/are [str1 str2 result] (= result (scramble? str1 str2))
+      "rekqodlw" "world" true
+      "cedewaraaossoqqyt" "codewars" true
+      "katas" "steak" false)))
